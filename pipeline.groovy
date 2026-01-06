772636ce312adc4fbfcfe2a366f171f0af9d3b14
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-06 16:25:17 +0530

    test-stage-added

--- pipeline.groovy
+++ pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
         stage('test-stage') {
             steps {
                
-             sh '''mvn clean verify sonar:sonar \\
+             sh '''/opt/maven/bin/mvn clean verify sonar:sonar \\
   -Dsonar.projectKey=new-studentapp \\
   -Dsonar.host.url=http://18.119.103.150:9000 \\
   -Dsonar.login=cb3a8ed37f0d155447ddaec6db9516b9b05ea07f'''
