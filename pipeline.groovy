4eb43f91d3f10c0f987a559dd99439ccc20f4acb
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-07 15:44:14 +0530

    vpc-module-added

--- pipeline.groovy
+++ pipeline.groovy
@@ -17,7 +17,7 @@ pipeline {
                
              sh '''/opt/maven/bin/mvn clean verify sonar:sonar \\
   -Dsonar.projectKey=new-studentapp \\
-  -Dsonar.host.url=http://18.119.103.150:9000 \\
+  -Dsonar.host.url=http://13.58.131.235:9000 \\
   -Dsonar.login=cb3a8ed37f0d155447ddaec6db9516b9b05ea07f'''
             }
         }
